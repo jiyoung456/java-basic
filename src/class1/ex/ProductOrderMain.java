b42176ea6f9b9f8d7d69ab050c855d7280894b5b
jiyoung <jjiyoung0421@gmail.com>
2025-09-01 16:33:27 +0900

    강사 풀이: 배열을 먼저 선언하고 각 상품을 배열에 할당

--- src/class1/ex/ProductOrderMain.java
+++ src/class1/ex/ProductOrderMain.java
@@ -3,22 +3,26 @@ package class1.ex;
 public class ProductOrderMain {
 
     public static void main(String[] args) {
+
+        ProductOrder[] products = new ProductOrder[3];
+
         ProductOrder tofu = new ProductOrder();
         tofu.productName = "두부";
         tofu.price = 2000;
         tofu.quantity = 2;
+        products[0] = tofu;
 
         ProductOrder kimchi = new ProductOrder();
         kimchi.productName = "김치";
         kimchi.price = 5000;
         kimchi.quantity = 1;
+        products[1] = kimchi;
 
         ProductOrder coke = new ProductOrder();
         coke.productName = "콜라";
         coke.price = 1500;
         coke.quantity = 2;
-
-        ProductOrder[] products = {tofu, kimchi, coke};
+        products[2] = coke;
 
         int totlaPrice = 0;
 
